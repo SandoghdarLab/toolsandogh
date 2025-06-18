ae0226574ccba4efb9e5878e81094b041ab3404d
Marco Heisig <marco.heisig@fau.de>
2025-06-18 13:38:07 +0200

    Define pre-commit hooks.

--- toolsandogh/tests/test_iscat_analysis.py
+++ toolsandogh/tests/test_iscat_analysis.py
@@ -1,8 +1,10 @@
-from toolsandogh.iscat_analysis import SharedArray, Analysis
-from multiprocessing import Pool
-import numpy as np
 import itertools
 import math
+from multiprocessing import Pool
+
+import numpy as np
+
+from toolsandogh.iscat_analysis import SharedArray
 
 
 def test_SharedArray():
@@ -10,7 +12,6 @@ def test_SharedArray():
     shapes: list[tuple[int, ...]] = [(2,), (2, 3), (2, 3, 5)]
     for dtype in dtypes:
         for shape in shapes:
-
             # Test individual array access
             sa = SharedArray(shape, dtype=dtype)
             indices = itertools.product(*[list(range(dim)) for dim in shape])
@@ -25,9 +26,13 @@ def test_SharedArray():
             # Test concurrent access
             size = math.prod(shape)
             with Pool(processes=size) as pool:
+
                 def initialize(array, index, value):
                     array[*index] = value
-                pool.starmap(initialize, [(sa, i, dtype(n)) for n, i in enumerate(indices)])
+
+                pool.starmap(
+                    initialize, [(sa, i, dtype(n)) for n, i in enumerate(indices)]
+                )
             for n, index in enumerate(indices):
                 value = dtype(n)
                 assert sa[*index] == value
